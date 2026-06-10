d189151b387b14443b459d75e427a47ac99b1198
angger <anggeridho@gmail.com>
2026-06-10 08:22:20 +0700

    Fix script

--- scripts/homelab-2/adguardSSL.sh
+++ scripts/homelab-2/adguardSSL.sh
@@ -3,4 +3,4 @@ HOMELAB="/root/homelab-infrastructure/scripts"
 source ${HOMELAB}/vault.sh && vault
 
 docker run --rm -v /mnt/disk1/adguard/acme:/acme.sh -v /mnt/disk1/adguard/ssl:/ssl -e CF_Token="${CF_TOKEN}" neilpang/acme.sh --cron
-docker service update --force adguard_adguardhome
+docker service update --force homelab_adguardhome
